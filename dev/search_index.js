fe9e838c98397fc894905ef88a62fea880513864
Documenter.jl <documenter@juliadocs.github.io>
2023-10-17 05:04:37 +0000

    build based on 2daaf91

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GenSquareDimNums","category":"page"},{"location":"#GenSquareDimNums","page":"Home","title":"GenSquareDimNums","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GenSquareDimNums.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GenSquareDimNums]","category":"page"}]
+}
